22c5bc13b02a4e7da96812cc667d0b6c3e2c4825
HukakHePak <valsidalvonilb@outlook.com>
2022-12-30 17:47:00 +0400

    config: remove semantic

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "semantic-ui-css/semantic.min.css";
-import "rsuite/dist/rsuite.min.css";
 import { StoreProvider, RootStore } from "contexts/RootStoreContext";
 import { RouterProvider } from "react-router-dom";
-import "scss/root.scss";
-// UIKit import
 import "uikit/dist/css/uikit.min.css";
+import "rsuite/dist/rsuite.min.css";
+import "scss/root.scss";
 import UIkit from "uikit";
 import Icons from "uikit/dist/js/uikit-icons";
 import Router from "./Router";
 
-// loads the Icon plugin
 UIkit.use(Icons);
 
 const container = document.getElementById("root");
