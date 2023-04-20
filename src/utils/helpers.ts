78707749eb65fbb141ed20108d0573d727752849
Pail4 <73517684+Pail4@users.noreply.github.com>
2023-04-20 23:34:04 +0400

    roles => UserType

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,8 @@
-import { User, roles as role } from "stores/models/user";
+import { User, UserType } from "stores/models/user";
 
 export const isAccessed = (roles: string[], user: User) =>
-  roles.includes(user.type) || roles.length === 0 || roles[0] === role.unauth;
+  roles.includes(user.type) ||
+  roles.length === 0 ||
+  roles[0] === UserType.unauth;
 
 export function more() {}
