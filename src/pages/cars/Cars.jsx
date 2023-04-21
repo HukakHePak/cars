eb525f1545cc042764d618411605570c760dc0e0
HukakHePak <valsidalvonilb@outlook.com>
2023-04-21 09:38:00 +0400

    remove consts

--- src/pages/cars/Cars.jsx
+++ src/pages/cars/Cars.jsx
@@ -3,24 +3,22 @@ import useStore from "hooks/useStore";
 import { observer } from "mobx-react-lite";
 import React from "react";
 import { FlexboxGrid } from "rsuite";
-import { roles } from "utils/consts";
+import { UserType } from "stores/models/user";
 import CarD from "./CarD";
 import Create from "./Create";
 import style from "./style";
 
+const { manager, admin } = UserType;
+
 function Cars() {
   const { cars } = useStore();
-
-  // useEffect(() => {
-  //   cars.load();
-  // }, []);
 
   return (
     <FlexboxGrid
       justify={cars.list.length < 2 ? "start" : "center"}
       className={style.cars}
     >
-      {can(<Create />, [roles.manager, roles.admin])}
+      {can(<Create />, [manager, admin])}
       {cars.list?.map((car) => (
         <CarD key={car.id} car={car} />
       ))}
